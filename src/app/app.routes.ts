3ccf563d223ff6c9d4bac890bda58ec7ffd6ad57
Thanathat Pinthu <thanathatp2003@gmail.com>
2026-05-24 04:06:59 +0700

    feat: sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ListProduct } from './pages/product/list-product/list-product';
+import { ListCategory } from './pages/category/list-category/list-category';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "products",
+        pathMatch: "full"
+    },
+    {
+        path: "products",
+        component: ListProduct
+    },
+    {
+        path: "categories",
+        component: ListCategory
+    }
+];
